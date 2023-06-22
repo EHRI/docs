7f2dc35c2b8800efdc2440df4d29e0b68c318e66
Mike Bryant <mikesname@gmail.com>
2023-06-22 19:15:31 +0100

    Ugh, fix the build

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,6 @@
 
 import sys
 import os
-import datetime
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -45,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'EHRI Documentation'
-copyright = f'{datetime.date.today().year}, The EHRI Consortium'
+copyright = u'2023, The EHRI Consortium'
 domain = "documentation.ehri-project.eu"
 
 # The version info for the project you're documenting, acts as replacement for
