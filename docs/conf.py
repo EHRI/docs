2560ed6463e75b6199100c5a973585a385e2805d
Mike Bryant <mikesname@gmail.com>
2023-06-22 19:07:09 +0100

    Fix copyright date

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@
 
 import sys
 import os
+import datetime
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -44,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'EHRI Documentation'
-copyright = u'2021, The EHRI Consortium'
+copyright = f'{datetime.date.today().year}, The EHRI Consortium'
 domain = "documentation.ehri-project.eu"
 
 # The version info for the project you're documenting, acts as replacement for
